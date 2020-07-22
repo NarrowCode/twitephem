4114b231c1b5c4b2a81d2c015bc3356f69816736
Andreas Steinkellner <ASteinkellner1@gmail.com>
2020-07-22 21:09:45 +0200

    First working version with delete and delete history.

--- twitephem.py
+++ twitephem.py
@@ -2,10 +2,13 @@
 import twitter
 import json
 import time
+from datetime import datetime
 from _api import api_key, api_secret, acc_token, acc_secret
 
-version = "1.0"
-print("TWITEPHEM v%s" %  version)
+VERSION = "1.0"
+DATE_FORMAT = '%d-%m-%Y'
+
+print("TWITEPHEM v%s" % VERSION)
 print("Connecting to twitter API.")
 api = twitter.Api(consumer_key=api_key,
                   consumer_secret=api_secret,
@@ -19,15 +22,47 @@ if api:
     print("         dh    - Delete tweets & save history")
     sel = str(raw_input("Enter your selection and press enter: "))
     if(sel == "d" or sel == "dh"):
-        statuses = api.GetUserTimeline(count=200)
-        outf = open("history.txt","a")
-        i = 1 
-        for status in statuses:
-            print(i, status.id, status.created_at_in_seconds)
+        start_date = str(raw_input("Please specify a start date in the form [DD-MM-YYYY]. " \
+                                   "Tweets after this date will be deleted: "))
+        start_date_obj = datetime.strptime(start_date, DATE_FORMAT)
+        end_date = str(raw_input("Please specify a end date in the form [DD-MM-YYYY]. " \
+                                 "Tweets after this date will not be deleted. " \
+                                 "Type [now] to delete everything up to the current day: "))
+        if (end_date == "now"):
+            end_date_obj = datetime.now() 
+        else:
+            end_date_obj = datetime.strptime(end_date, DATE_FORMAT)
+
+        print("If you continue, every tweet between {0} and {1} will be deleted.".format(
+              start_date_obj.strftime(DATE_FORMAT), end_date_obj.strftime(DATE_FORMAT)))
+        confirmed = str(raw_input("Continue? (type yes/no): "))
+        if (confirmed == "yes" or confirmed == "Yes"):
+            statuses = api.GetUserTimeline(count=200)
             if (sel == "dh"):
-                outf.write("{s_id};{s_t};{s_txt}\n".format(s_id = status.id,
-                           s_t = time.asctime(status.created_at_in_seconds), 
-                           s_txt = status.text.encode("utf-8")))
-            i += 1
+                fname = "history_{0}_{1}.txt".format(start_date_obj.strftime(DATE_FORMAT),
+                        end_date_obj.strftime(DATE_FORMAT))
+                outf = open(fname, "a")
+            latest_id = "" 
+            i = 1
+            while len(statuses) > 1:
+                for status in statuses:
+                    creat_dattim = datetime.fromtimestamp(status.created_at_in_seconds).strftime(DATE_FORMAT)
+                    creat_dattim_obj = datetime.strptime(creat_dattim, DATE_FORMAT)
+                    if (sel == "dh"):
+                        outf.write("{s_id};{s_t};{s_txt}\n".format(s_id = status.id,
+                                   s_t = time.asctime(time.gmtime(status.created_at_in_seconds)), 
+                                   s_txt = status.text.encode("utf-8")))
+                    i += 1
+                    latest_id = status.id
+                    if ((sel == "dh" or sel == "d") 
+                    and (creat_dattim_obj >= start_date_obj and creat_dattim_obj < end_date_obj)): 
+                        print(i, status.id, creat_dattim, "destroy=YES") 
+                        api.DestroyStatus(latest_id)
+                    else:
+                        print(i, status.id, creat_dattim, "destroy=NO") 
+
+                statuses = api.GetUserTimeline(count = 200, max_id = latest_id)
+        else:
+            print("Aborted.")
 else:
     print("nay")
